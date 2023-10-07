3c4293529acb865229eba819e8aae0d2e3dcaa4a
Antony Jaison <antonyjaison639@gmail.com>
2023-10-07 22:20:07 +0530

    mobile ui completed for projects page

--- components/ProjectInfo.jsx
+++ components/ProjectInfo.jsx
@@ -6,19 +6,18 @@ const ProjectInfo = () => {
         <div className={styles.wrapper}>
             <div className={styles.header}>
                 <div className={styles.tab}>
-                    <p><span className={styles.folder_name}>// personal info /</span><span> LOEM</span></p>
+                    <p><span className={styles.folder_name}>// personal info /</span> LOEM</p>
                     <img src="/icons/close.svg" alt="close-icon" />
                 </div>
             </div>
 
             <div className={styles.body}>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-                <ProjectCard/>
-          
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
             </div>
         </div>
     )
