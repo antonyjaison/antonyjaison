d7cc0f16514ce31cc7b46846953196a5cbf2f50b
antonyjaison <antonyjaison639@gmail.com>
2023-10-11 00:56:48 +0530

    added snake game background

--- components/SnakeGame.jsx
+++ components/SnakeGame.jsx
@@ -1,8 +1,28 @@
+'use client'
+
 import styles from '@styles/snakeGame.module.scss'
+import { useState } from 'react'
 
 const SnakeGame = () => {
+
     return (
-        <div className={styles.wrapper}>SnakeGame</div>
+        <div className={styles.wrapper}>
+            <img className={styles.img_1} src="/icons/snake_corner_img.svg" alt="" />
+            <img className={styles.img_2} src="/icons/snake_corner_img.svg" alt="" />
+            <img className={styles.img_3} src="/icons/snake_corner_img.svg" alt="" />
+            <img className={styles.img_4} src="/icons/snake_corner_img.svg" alt="" />
+            <div className={styles.game_wrapper}>
+
+                <div className={styles.game_section}>
+
+                </div>
+
+                <div className={styles.control_section}>
+
+                </div>
+
+            </div>
+        </div>
     )
 }
 
