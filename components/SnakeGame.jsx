11bcd3ad6a910c8935cee4703f4ae529b9b44191
antonyjaison <antonyjaison639@gmail.com>
2023-10-12 17:18:53 +0530

    added snake game ui and working with minor issues

--- components/SnakeGame.jsx
+++ components/SnakeGame.jsx
@@ -1,9 +1,12 @@
-'use client'
+"use client"
 
 import styles from '@styles/snakeGame.module.scss'
-import { useState } from 'react'
+import { Snake } from '@json2d/react-snake-lib'
 
 const SnakeGame = () => {
+
+    const bgColor = "rgba(1, 22, 39, 0.84)"
+    const snakeColor  = "#43D9AD"
 
     return (
         <div className={styles.wrapper}>
@@ -14,6 +17,47 @@ const SnakeGame = () => {
             <div className={styles.game_wrapper}>
 
                 <div className={styles.game_section}>
+
+                    {/* <Snake
+                        startOnLoad
+                        initialSnake={[
+                            { x: 2, y: 0 },
+                            { x: 2, y: 1 },
+                            { x: 2, y: 2 },
+                            { x: 2, y: 3 },
+                            { x: 2, y: 4 },
+                        ]}
+                        initialDirection="down"
+                        width="100%"
+                        height="100%"
+                        bgColor={bgColor}
+                        innerBorderColor={bgColor}
+                        snakeSpeed={100}
+                        borderColor={bgColor}
+                        snakeColor={snakeColor}
+                        snakeHeadColor={snakeColor}
+                        appleColor="tomato"
+                        borderRadius={5}
+                        snakeHeadRadius={1}
+                        borderWidth={0}
+                        shakeBoard={true}
+                        boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
+                        size={20}
+                        startGameText="Start Game"
+                        startButtonStyle={{
+                            color: "white",
+                            padding: "6px 20px",
+                            backgroundColor: "rgba(1, 22, 39, 0.84)",
+                            borderRadius: "10px",
+                            fontSize: "17px",
+                            fontWeight: "600",
+                            cursor: "pointer"
+                        }}
+                        startButtonHoverStyle={{
+                            backgroundColor: "#43D9AD"
+                        }}
+                        noWall={true}
+                    /> */}
 
                 </div>
 
