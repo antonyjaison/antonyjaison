06e22365693192ad39b7d7ada011e27d34030590
Antony Jaison <antonyjaison639@gmail.com>
2023-11-01 15:50:59 +0530

    added user os, browser and time details to the sheet

--- app/api/track/route.js
+++ app/api/track/route.js
@@ -1,18 +1,25 @@
 import { NextResponse } from "next/server";
-import { google } from "googleapis"
 import connectToSpreadSheet from "@utils/connectToSpreadSheet";
 
 
 export async function POST(req, res) {
 
     const { ip } = await req.json()
-    console.log(ip)
 
     const apiRes = await fetch(`https://ipinfo.io/${ip}/json?token=078ba5fb764a89`)
     const data = await apiRes.json()
-    console.log(data) 
 
-    const spreadsheetRes = await connectToSpreadSheet(process.env.formTrackID, [data.ip, data.city, data.region, data.country, data.loc, data.org, data.postal, data.timezone])
+    // get requested clients os and browser
+    const userAgent = req.headers.get("user-agent")
+    const os = userAgent.split(") ")[0].split(" (")[1]
+    const browser = userAgent.split(") ")[0].split(" (")[0] 
+
+    // get current date and time
+    const date = new Date()
+    const currentDate = date.toLocaleDateString()
+    const currentTime = date.toLocaleTimeString()
+
+    const spreadsheetRes = await connectToSpreadSheet(process.env.formTrackID, [data.ip, data.city, data.region, data.country, data.loc, data.org, data.postal, data.timezone,currentDate,currentTime,os,browser])
 
     if (spreadsheetRes) {
         return NextResponse.json({ status: 200, body: { message: "success", status: true } });
